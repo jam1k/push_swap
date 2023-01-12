bcc665359cdabed9ed386f90e8fac9fc1a32c07c
Jamilya Shestovskaya <jshestov@c3r2p4.hive.fi>
2023-01-12 14:21:50 +0200

    working reverse_rotate

--- operations3.c
+++ operations3.c
@@ -6,7 +6,7 @@
 /*   By: jshestov <jshestov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 10:50:59 by jshestov          #+#    #+#             */
-/*   Updated: 2023/01/12 13:14:22 by jshestov         ###   ########.fr       */
+/*   Updated: 2023/01/12 14:21:21 by jshestov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 void	reverse_rotate_a(t_stack **a_stack)
 {
-	t_stack	*last;
-	//t_stack	*prev;
+	t_stack	*current;
+	t_stack	*prev;
 
-	if (!*a_stack || (*a_stack)->next == NULL)
-		return ;
-	last = ft_stacklast(*a_stack);
-	last->next = (*a_stack);
+	current = *a_stack;
+	prev = NULL;
+	while (current->next != NULL)
+	{
+		prev = current;
+		current = current->next;
+	}
+	current->next = *a_stack;
+	prev->next = NULL;
+	*a_stack = current;
 }
 
 void	reverse_rotate_b(t_stack **b_stack)
